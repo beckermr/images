f3bb162cb298dac8263627509289c4ade5a422de
Matthew R Becker <becker.mr@gmail.com>
2014-09-29 15:36:48 -0700

    first commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+import os
+import glob
+from distutils.core import setup
+
+setup(name="images", 
+      version="0.1.0",
+      description="biggles code to plot images",
+      license = "GPL",
+      author="Erin Scott Sheldon",
+      author_email="erin.sheldon@gmail.com",
+      packages=['images'])
+
+
